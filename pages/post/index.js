cefe703e1bb31ebdb7952316e0631ee41e843433
Gilad Levi <gilad.x@gmail.com>
2024-03-05 19:10:19 +0200

    Commit from TeleportHQ

---
+++ pages/post/index.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/react-components'
+import PropTypes from 'prop-types'
+
+import postPageInitialPropsTq1bResource from '../../resources/post-page-initial-props-tq_1b'
+
+const Post = (props) => {
+  return (
+    <>
+      <div className="post-container">
+        <Head>
+          <title>Post - SpiderNet Nextjs new</title>
+          <meta property="og:title" content="Post - SpiderNet Nextjs new" />
+          <meta name="robots" content="noindex" />
+        </Head>
+        <DataProvider
+          renderSuccess={(params) => (
+            <>
+              <Repeater
+                items={params}
+                renderItem={(PostEntities) => (
+                  <>
+                    <div className="post-container1">
+                      <h1>{PostEntities?.title?.rendered}</h1>
+                    </div>
+                  </>
+                )}
+              />
+            </>
+          )}
+          initialData={props.postEntities}
+          persistDataDuringLoading={true}
+          key={props?.pagination?.page}
+        />
+      </div>
+      <style jsx>
+        {`
+          .post-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .post-container1 {
+            gap: 12px;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+Post.defaultProps = {
+  postEntities: [],
+}
+
+Post.propTypes = {
+  postEntities: PropTypes.array,
+}
+
+export default Post
+
+export async function getStaticProps(context) {
+  try {
+    const response = await postPageInitialPropsTq1bResource({
+      ...context?.params,
+    })
+    if (!response) {
+      return {
+        notFound: true,
+      }
+    }
+    return {
+      props: {
+        postEntities: response,
+        ...response?.meta,
+      },
+      revalidate: 60,
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    }
+  }
+}
---
+++ pages/post/page/[page].js
@@ -0,0 +1,121 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/react-components'
+import PropTypes from 'prop-types'
+
+import postPageInitialPropsTqRJResource from '../../../resources/post-page-initial-props-tq_r-j'
+import postPageInitialPathsTqCwResource from '../../../resources/post-page-initial-paths-tq_cw'
+
+const Post1 = (props) => {
+  return (
+    <>
+      <div className="post1-container">
+        <Head>
+          <title>Post - SpiderNet Nextjs new</title>
+          <meta property="og:title" content="Post - SpiderNet Nextjs new" />
+          <meta name="robots" content="noindex" />
+        </Head>
+        <DataProvider
+          renderSuccess={(params) => (
+            <>
+              <Repeater
+                items={params}
+                renderItem={(PostEntities) => (
+                  <>
+                    <div className="post1-container1">
+                      <h1>{PostEntities?.title?.rendered}</h1>
+                    </div>
+                  </>
+                )}
+              />
+            </>
+          )}
+          initialData={props.postEntities}
+          persistDataDuringLoading={true}
+          key={props?.pagination?.page}
+        />
+      </div>
+      <style jsx>
+        {`
+          .post1-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .post1-container1 {
+            gap: 12px;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+Post1.defaultProps = {
+  postEntities: [],
+}
+
+Post1.propTypes = {
+  postEntities: PropTypes.array,
+}
+
+export default Post1
+
+export async function getStaticProps(context) {
+  try {
+    const response = await postPageInitialPropsTqRJResource({
+      ...context?.params,
+    })
+    if (!response) {
+      return {
+        notFound: true,
+      }
+    }
+    return {
+      props: {
+        postEntities: response,
+        ...response?.meta,
+      },
+      revalidate: 60,
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  try {
+    const response = await postPageInitialPathsTqCwResource({})
+    const headers = Object.fromEntries(response)
+    const totalCount = headers?.['x-wp-total']
+    const pagesCount = Math.ceil(totalCount / 10)
+    return {
+      paths: Array.from(
+        {
+          length: pagesCount,
+        },
+        (_, i) => ({
+          params: {
+            page: (i + 1).toString(),
+          },
+        })
+      ),
+      fallback: 'blocking',
+    }
+  } catch (error) {
+    return {
+      paths: [],
+      fallback: 'blocking',
+    }
+  }
+}
